4c2dc5a972f877388b55269f691a06f0b7001090
Vansh <vansh.gagan@uzh.ch>
2026-03-17 19:08:22 +0100

    implement TCP port scanner with worker-pools to increase working speed (40 workers at the moment)

--- main.go
+++ main.go
@@ -3,24 +3,111 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
+	"sync"
 	"time"
 )
 
-func main() {
+func worker(jobs chan int, results chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	target_ip := "127.0.0.1"
-	if len(os.Args) < 2 {
-		fmt.Print("Usage: sudo go run main.go <port> \n")
-	}
-	port := os.Args[1]
-	address := net.JoinHostPort(target_ip, fmt.Sprintf("%s", port))
 
-	conn, err := net.DialTimeout("tcp", address, 2*time.Second)
-	if err != nil {
-		fmt.Printf("Port %s is closed \n", port)
-		return
-	}
-	defer conn.Close()
-	fmt.Printf("Port: %s is open \n", port)
+	for port := range jobs {
+		address := net.JoinHostPort(target_ip, fmt.Sprintf("%d", port))
 
+		conn, err := net.DialTimeout("tcp", address, 2*time.Second)
+		if err != nil {
+			continue
+		}
+		conn.Close()
+		results <- port
+
+	}
+}
+
+func main() {
+
+	var portNames = map[int]string{
+		20:    "FTP Data",
+		21:    "FTP",
+		22:    "SSH",
+		23:    "Telnet",
+		25:    "SMTP",
+		53:    "DNS",
+		80:    "HTTP",
+		110:   "POP3",
+		119:   "NNTP",
+		123:   "NTP",
+		137:   "NetBIOS",
+		139:   "NetBIOS",
+		143:   "IMAP",
+		161:   "SNMP",
+		179:   "BGP",
+		389:   "LDAP",
+		443:   "HTTPS",
+		445:   "SMB",
+		465:   "SMTPS",
+		500:   "ISAKMP",
+		587:   "SMTP (modern)",
+		631:   "CUPS",
+		993:   "IMAPS",
+		995:   "POP3S",
+		1433:  "MSSQL",
+		1521:  "Oracle",
+		1723:  "PPTP",
+		1883:  "MQTT",
+		2049:  "NFS",
+		2083:  "cPanel",
+		2181:  "Zookeeper",
+		2375:  "Docker",
+		3000:  "Dev Server",
+		3306:  "MySQL",
+		3389:  "RDP",
+		4444:  "Metasploit",
+		5000:  "Flask",
+		5432:  "PostgreSQL",
+		5601:  "Kibana",
+		5672:  "RabbitMQ",
+		5900:  "VNC",
+		6379:  "Redis",
+		6443:  "Kubernetes",
+		7001:  "WebLogic",
+		7474:  "Neo4j",
+		7687:  "Neo4j Bolt",
+		8000:  "HTTP Alt",
+		8080:  "HTTP Alt",
+		8443:  "HTTPS Alt",
+		9000:  "Dev Server",
+		9090:  "Prometheus",
+		9200:  "Elasticsearch",
+		27017: "MongoDB",
+	}
+
+	jobs := make(chan int)
+	results := make(chan int)
+
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 40; i++ {
+		wg.Add(1)
+		go worker(jobs, results, &wg)
+	}
+
+	for port := 1; port <= 65535; port++ {
+		jobs <- port
+	}
+	close(jobs)
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for res := range results {
+		name, exists := portNames[res]
+		if exists {
+			fmt.Printf("Port %d open --> %s\n", res, name)
+		} else {
+			fmt.Printf("Port %d open\n", res)
+		}
+	}
 }
