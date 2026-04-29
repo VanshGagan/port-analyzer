66144667452eeb847dc2f19b06e3a7ff0cafcb21
Vansh <vansh.gagan@uzh.ch>
2026-04-29 11:23:04 +0200

    added: connection via tls for https (port 443)

--- banner/grabber.go
+++ banner/grabber.go
@@ -1,6 +1,7 @@
 package banner
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net"
 	"strings"
@@ -11,17 +12,32 @@ import (
 // read the banner
 func Banner_grabber(port int, target string, wg *sync.WaitGroup) {
 	defer wg.Done()
+	var conn net.Conn
+	var err error
 
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", target, port), 1*time.Second)
-	if err != nil {
-		return
+	if port == 443 {
+		dial_conf := &net.Dialer{Timeout: 1 * time.Second}
+		tls_conf := &tls.Config{InsecureSkipVerify: true}
+
+		conn, err = tls.DialWithDialer(dial_conf, "tcp", fmt.Sprintf("%s:%d", target, port), tls_conf)
+		if err != nil {
+			return
+		}
+	} else {
+
+		conn, err = net.DialTimeout("tcp", fmt.Sprintf("%s:%d", target, port), 1*time.Second)
+		if err != nil {
+			return
+		}
 	}
-	if port == 80 || port == 8000 || port == 8080 {
+
+	if port == 80 || port == 8000 || port == 8080 || port == 443 {
 		fmt.Fprintf(conn, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", target)
 	}
+
 	d := make([]byte, 1024)
 
-	conn.SetReadDeadline(time.Now().Add(500 * time.Millisecond))
+	conn.SetReadDeadline(time.Now().Add(1500 * time.Millisecond))
 
 	n, err := conn.Read(d)
 	if err != nil {
